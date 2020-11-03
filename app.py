db88467b58b41d476f51f1622bba9555a5b07391
Matias <matiaswpartanen@gmail.com>
2020-11-03 14:58:22 +0200

    Basic login and logout with login- and homepage

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    
+    session["username"] = username
+    return redirect("/home")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
---
+++ sessioncontroller.py
@@ -0,0 +1,4 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+
