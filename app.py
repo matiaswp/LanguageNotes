03be80021e51f8d1c87689d13593f9820ab0e457
Matias <matiaswpartanen@gmail.com>
2020-11-04 16:00:44 +0200

    Added Heroku config

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__)
---
+++ db.py
@@ -0,0 +1,3 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
